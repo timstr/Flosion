3d978b31924b93b9ca98b740af19b74342ab352d
timstr <f2a0b@ugrad.cs.ubc.ca>
2017-05-02 15:55:15 -0700

    Removed redundant labels

--- Flosion/wavegenUI.h
+++ Flosion/wavegenUI.h
@@ -12,9 +12,7 @@ namespace fui {
 			addNumberInput(new NumberInput(&wavegen.frequency.input, this, "Frequency", {-5, 5}));
 			addNumberInput(new NumberInput(&wavegen.wavefunction.input, this, "Wave Function", {-5, 40}));
 			addNumberOutput(new NumberOutput(&wavegen.phase, this, {60, -5}));
-			addChildWindow(new ui::Text("frequency", fui::getFont()), {25, 5});
-			addChildWindow(new ui::Text("wavefunction", fui::getFont()), {25, 40});
-			addChildWindow(new ui::Text("WaveGenerator", fui::getFont()), {40, 60});
+			addChildWindow(new ui::Text("WaveGenerator", fui::getFont()), {25, 25});
 		}
 
 		private:
