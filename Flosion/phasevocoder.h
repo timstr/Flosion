409fd0cab811bb8a5bf450012928505b9185f7e2
timstr <f2a0b@ugrad.cs.ubc.ca>
2017-11-20 01:59:48 -0800

    A much better phase vocoder

--- Flosion/phasevocoder.h
+++ Flosion/phasevocoder.h
@@ -7,59 +7,30 @@
 
 namespace musical {
 
-	// TODO: overlap and add for (hopefully) clean sound
 	// TODO: separate left and right channels for stereo
 
 	struct PhaseVocoderState : State {
 		using State::State;
 
 		void reset() override {
-			offset = 1.0f;
+			offset = 0.0f;
+			phase = 0;
 			for (int i = 0; i < CHUNK_SIZE; i++){
-				buffer[i] = Sample(0, 0);
+				inbuffer1[i] = Sample(0, 0);
+				inbuffer2[i] = Sample(0, 0);
 				prev[i] = 0.0f;
 				next[i]= 0.0f;
 				phase_diffs[i] = 0.0f;
 				phase_acc[i] = 0.0f;
-				cbuffer[i] = 0.0f;
-			}
-		}
-
-		// assumes buffer has been filled with next chunk of audio data
-		void swap(){
-			// shuffle freq spectrums over
-			for (int i = 0; i < CHUNK_SIZE; i++){
-				prev[i] = next[i];
-				next[i] = buffer[i].l * getHannWindow(i); // TODO: non-stereo, right here
-			}
-
-			// compute next spectrum from new audio data
-			fft(next, CHUNK_SIZE);
-
-			for (int i = 0; i < CHUNK_SIZE; i++){
-				phase_diffs[i] = std::arg(next[i]) - std::arg(prev[i]);
-			}
-		}
-
-		void synthesize(){
-			for (int i = 0; i < CHUNK_SIZE; i++){
-				// linear interpolation of magnitude
-				float mag = std::abs(prev[i]) * (1.0f -  offset) + std::abs(next[i]) * offset;
-
-				// phase is accumulated
-				phase_acc[i] = fmod(phase_acc[i] + phase_diffs[i], 2.0f * 3.141592654f);
-
-				cbuffer[i] = mag * complex(cos(phase_acc[i]), sin(phase_acc[i]));
-			}
-			ifft(cbuffer, CHUNK_SIZE);
-			for (int i = 0; i < CHUNK_SIZE; i++){
-				buffer[i].l = cbuffer[i].real();
-				buffer[i].r = buffer[i].l;
+				outbuffer[i] = 0.0f;
+				carryover[i] = Sample(0, 0);
 			}
 		}
 
 		// for analysis
-		Sample buffer[CHUNK_SIZE];	// for input audio data
+		int phase;
+		Sample inbuffer1[CHUNK_SIZE];	// for input audio data
+		Sample inbuffer2[CHUNK_SIZE];	// for input audio data
 		complex prev[CHUNK_SIZE];	// phase/amp of last window
 		complex next[CHUNK_SIZE];	// phase/amp of next window
 
@@ -67,7 +38,8 @@ namespace musical {
 		// for re-synthesis
 		float phase_diffs[CHUNK_SIZE];
 		float phase_acc[CHUNK_SIZE];
-		complex cbuffer[CHUNK_SIZE];
+		complex outbuffer[CHUNK_SIZE];
+		Sample carryover[CHUNK_SIZE];
 
 		float offset;
 	};
@@ -78,21 +50,175 @@ namespace musical {
 		}
 
 		void renderChunk(Sample* buffer, PhaseVocoderState* state){
-			// get next spectrum if needed
-			if (state->offset >= 1.0f){
-				state->offset -= 1.0f;
+			float speed = timespeed.getValue(state, 1.0f);
 
-				input.getNextChunk(state->buffer, state);
-				state->swap();
+			// previous 1, 2, and 3
+			for (int i = 0; i < CHUNK_SIZE; i++){
+				buffer[i] = state->carryover[i];
 			}
 
-			state->synthesize();
+			// 0
+			getNextFrame(speed, state);
+			for (int i = 0; i < CHUNK_SIZE; i++){
+				float val = state->outbuffer[i].real() * getHannWindow(i);
+				buffer[i].l += val;
+				buffer[i].r += val;
+				state->carryover[i] = Sample(0, 0);
+			}
+			
+			// 1
+			getNextFrame(speed, state);
+			for (int i = 0; i < CHUNK_SIZE * 3 / 4; i++){
+				float val = state->outbuffer[i].real() * getHannWindow(i);
+				buffer[i + CHUNK_SIZE / 4].l += val;
+				buffer[i + CHUNK_SIZE / 4].r += val;
+			}
+			for (int i = 0; i < CHUNK_SIZE / 4; i++){
+				float val = state->outbuffer[i + CHUNK_SIZE * 3 / 4].real() * getHannWindow(i + CHUNK_SIZE * 3 / 4);
+				state->carryover[i].l += val;
+				state->carryover[i].r += val;
+			}
+
+			// 2
+			getNextFrame(speed, state);
+			for (int i = 0; i < CHUNK_SIZE / 2; i++){
+				float val = state->outbuffer[i].real() * getHannWindow(i);
+				buffer[i + CHUNK_SIZE / 2].l += val;
+				buffer[i + CHUNK_SIZE / 2].r += val;
+			}
+			for (int i = 0; i < CHUNK_SIZE / 2; i++){
+				float val = state->outbuffer[i + CHUNK_SIZE / 2].real() * getHannWindow(i + CHUNK_SIZE / 2);
+				state->carryover[i].l += val;
+				state->carryover[i].r += val;
+			}
+
+			// 3
+			getNextFrame(speed, state);
+			for (int i = 0; i < CHUNK_SIZE / 4; i++){
+				float val = state->outbuffer[i].real() * getHannWindow(i);
+				buffer[i + CHUNK_SIZE * 3 / 4].l += val;
+				buffer[i + CHUNK_SIZE * 3 / 4].r += val;
+			}
+			for (int i = 0; i < CHUNK_SIZE * 3 / 4; i++){
+				float val = state->outbuffer[i + CHUNK_SIZE / 4].real() * getHannWindow(i + CHUNK_SIZE / 4);
+				state->carryover[i].l += val;
+				state->carryover[i].r += val;
+			}
+		}
+
+		void swap(PhaseVocoderState* state){
+			for (int i = 0; i < CHUNK_SIZE; i++){
+				state->prev[i] = state->next[i];
+			}
+
+			if (state->phase == 0){
+				for (int i = 0; i < CHUNK_SIZE; i++){
+					state->next[i] = state->inbuffer1[i].l * getHannWindow(i);
+				}
+
+				state->phase = 1;
+			} else if (state->phase == 1){
+				for (int i = 0; i < CHUNK_SIZE * 3 / 4; i++){
+					state->next[i] = state->inbuffer1[i + CHUNK_SIZE / 4].l * getHannWindow(i);
+				}
+				input.getNextChunk(state->inbuffer2, state);
+				for (int i = 0; i < CHUNK_SIZE / 4; i++){
+					state->next[i + CHUNK_SIZE * 3 / 4] = state->inbuffer2[i].l * getHannWindow(i + CHUNK_SIZE * 3 / 4);
+				}
+
+				state->phase = 2;
+			} else if (state->phase == 2){
+				for (int i = 0; i < CHUNK_SIZE / 2; i++){
+					state->next[i] = state->inbuffer1[i + CHUNK_SIZE / 2].l * getHannWindow(i);
+				}
+				for (int i = 0; i < CHUNK_SIZE / 2; i++){
+					state->next[i + CHUNK_SIZE / 2] = state->inbuffer2[i].l * getHannWindow(i + CHUNK_SIZE / 2);
+				}
+
+				state->phase = 3;
+			} else if (state->phase == 3){
+				for (int i = 0; i < CHUNK_SIZE / 4; i++){
+					state->next[i] = state->inbuffer1[i + CHUNK_SIZE * 3 / 4].l * getHannWindow(i);
+				}
+				for (int i = 0; i < CHUNK_SIZE * 3 / 4; i++){
+					state->next[i + CHUNK_SIZE / 4] = state->inbuffer2[i].l * getHannWindow(i + CHUNK_SIZE / 4);
+				}
+
+				state->phase = 4;
+			} else if (state->phase == 4){
+				for (int i = 0; i < CHUNK_SIZE; i++){
+					state->next[i] = state->inbuffer2[i].l * getHannWindow(i);
+				}
+
+				state->phase = 5;
+			} else if (state->phase == 5){
+				for (int i = 0; i < CHUNK_SIZE * 3 / 4; i++){
+					state->next[i] = state->inbuffer2[i + CHUNK_SIZE / 4].l * getHannWindow(i);
+				}
+				input.getNextChunk(state->inbuffer1, state);
+				for (int i = 0; i < CHUNK_SIZE / 4; i++){
+					state->next[i + CHUNK_SIZE * 3 / 4] = state->inbuffer1[i].l * getHannWindow(i + CHUNK_SIZE * 3 / 4);
+				}
+
+				state->phase = 6;
+			} else if (state->phase == 6){
+				for (int i = 0; i < CHUNK_SIZE / 2; i++){
+					state->next[i] = state->inbuffer2[i + CHUNK_SIZE / 2].l * getHannWindow(i);
+				}
+				for (int i = 0; i < CHUNK_SIZE / 2; i++){
+					state->next[i + CHUNK_SIZE / 2] = state->inbuffer1[i].l * getHannWindow(i + CHUNK_SIZE / 2);
+				}
+
+				state->phase = 7;
+			} else if (state->phase == 7){
+				for (int i = 0; i < CHUNK_SIZE / 4; i++){
+					state->next[i] = state->inbuffer2[i + CHUNK_SIZE * 3 / 4].l * getHannWindow(i);
+				}
+				for (int i = 0; i < CHUNK_SIZE * 3 / 4; i++){
+					state->next[i + CHUNK_SIZE / 4] = state->inbuffer1[i].l * getHannWindow(i + CHUNK_SIZE / 4);
+				}
+
+				state->phase = 0;
+			}
+
+
+
+
+
+
+			fft(state->next, CHUNK_SIZE);
+			// cyclic shift
+			for (int i = 1; i < CHUNK_SIZE; i += 2){
+				state->next[i] *= -1.0f;
+			}
+			for (int i = 0; i < CHUNK_SIZE; i++){
+				state->phase_diffs[i] = std::arg(state->next[i]) - std::arg(state->prev[i]);
+			}
+		}
+
+		// write spectrum to outbuffer
+		void getNextFrame(float speed, PhaseVocoderState* state){
+			while (state->offset > 1.0f){
+				swap(state);
+				state->offset -= 1.0f;
+			}
 
 			for (int i = 0; i < CHUNK_SIZE; i++){
-				buffer[i] = state->buffer[i];
+				// interpolate magnitudes from prev to next
+				float mag = std::abs(state->prev[i]) * (1.0f - state->offset) + std::abs(state->next[i]) * state->offset;
+
+				// accumulate phases
+				state->phase_acc[i] = fmod(state->phase_acc[i] + state->phase_diffs[i], 2.0f * 3.141592654f);
+
+				state->outbuffer[i] = mag * complex(cos(state->phase_acc[i]), sin(state->phase_acc[i]));
 			}
 
-			float speed = timespeed.getValue(state, 1.0f);
+			// cyclic shift
+			for (int i = 1; i < CHUNK_SIZE; i += 2){
+				state->outbuffer[i] *= -1.0f;
+			}
+			ifft(state->outbuffer, CHUNK_SIZE);
+
 			state->offset += speed;
 		}
 
